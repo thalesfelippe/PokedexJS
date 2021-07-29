0fef7e57e811f3a1d2af3859492a0a2210b1b6e9
unknown <thales_so@hotmail.com>
2021-07-29 15:46:52 -0300

    validator finished

---
+++ JS/form.js
@@ -0,0 +1,80 @@
+let PokeValidator = {
+  handleSubmit:(event) => {
+      event.preventDefault();
+      let send = true;
+
+      let inputs = form.querySelectorAll('input');
+
+      PokeValidator.clearErrors();
+
+      for (let i = 0; i < inputs.length; i++) {
+          let input = inputs[i];
+          let check = PokeValidator.checkInput(input);
+          if(check !== true) {
+              send = false;
+              PokeValidator.showError(input, check);
+          }
+      }
+
+      if(send) {
+          form.submit();
+      }
+  },
+  checkInput:(input) => {
+      let rules = input.getAttribute('data-rules');
+
+      if(rules !== null) {
+          rules = rules.split(',');
+          for(let k in rules) {
+              let rDetails = rules[k].split('=');
+              switch(rDetails[0]) {
+                  case 'required':
+                      if(input.value == '') {
+                          return 'Este campo não pode ser vazio.';
+                      }
+                      break;
+
+                  case 'min':
+                      if(input.value.length < rDetails[1]) {
+                          return 'Este campo tem que ter '+rDetails[1]+' caracteres';
+                      }
+                      break;
+
+                  case 'email':
+                      if(input.value != '') {
+                          let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+                          if(!regex.test(input.value.toLowerCase())) {
+                              return 'Este e-mail não e válido.';
+                          }
+                      }
+                      break;
+              }
+          }
+      }
+
+      return true;
+  },
+  showError:(input, error) => {
+      input.style.borderColor = '#FF0000';
+
+      let errorElement = document.createElement('div');
+      errorElement.classList.add('error');
+      errorElement.innerHTML = error;
+
+      input.parentElement.insertBefore(errorElement, input.nextElementSibling);
+  },
+  clearErrors:() => {
+      let inputs = form.querySelectorAll('input');
+      for(let i = 0; i < inputs.length; i++) {
+          inputs[i].style = '';
+      }
+
+      let errorElements = document.querySelectorAll('.error');
+      for(let i = 0; i < errorElements.length; i++) {
+          errorElements[i].remove();
+      }
+  }
+};
+
+let form = document.querySelector('.pokevalidator');
+form.addEventListener('submit',PokeValidator.handleSubmit);
--- script.js
+++
@@ -1,8 +0,0 @@
-let P5Validator = {
-  handleSubmit:(event) => {
-    event.preventDefault();
-  }
-};
-
-let form = document.querySelector('p5validator');
-form.addEventListener('submit', P5Validator.handleSubmit);
