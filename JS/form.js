cdc308d803e67d851c9bb89a14aa8c47eedb5d8a
thalesfelippe <thales_so@hotmail.com>
2023-02-23 01:11:15 -0300

    Formulario aceitando validacao

--- JS/form.js
+++ JS/form.js
@@ -22,82 +22,85 @@ function exibeCadastro(){
 
     
 var PokeValidator = {
-  handleSubmit:(event) => {
-      event.preventDefault();
-      var send = true;
+    handleSubmit: (event) => {
+        event.preventDefault();
+        var send = true;
 
-      var inputs = form.querySelectorAll('input');
+        var inputs = event.target.querySelectorAll('input');
 
-      PokeValidator.clearErrors();
+        PokeValidator.clearErrors();
 
-      for (var i = 0; i < inputs.length; i++) {
-          var input = inputs[i];
-          var check = PokeValidator.checkInput(input);
-          if(check !== true) {
-              send = false;
-              PokeValidator.showError(input, check);
-          }
-      }
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            var check = PokeValidator.checkInput(input);
+            if (check !== true) {
+                send = false;
+                PokeValidator.showError(input, check);
+            }
+        }
 
-      if(send) {
-          form.submit();
-      }
-  },
-  checkInput:(input) => {
-      var rules = input.getAttribute('data-rules');
+        if (send) {
+            event.target.submit();
+        }
+    },
+    checkInput: (input) => {
+        var rules = input.getAttribute('data-rules');
 
-      if(rules !== null) {
-          rules = rules.split(',');
-          for(var k in rules) {
-              var rDetails = rules[k].split('=');
-              switch(rDetails[0]) {
-                  case 'required':
-                      if(input.value == '') {
-                          return 'This field cannot be empty.';
-                      }
-                      break;
+        if (rules !== null) {
+            rules = rules.split(',');
+            for (var k in rules) {
+                var rDetails = rules[k].split('=');
+                switch (rDetails[0]) {
+                    case 'required':
+                        if (input.value == '') {
+                            return 'This field cannot be empty.';
+                        }
+                        break;
 
-                  case 'min':
-                      if(input.value.length < rDetails[1]) {
-                          return 'This field must have '+rDetails[1]+' characters';
-                      }
-                      break;
+                    case 'min':
+                        if (input.value.length < rDetails[1]) {
+                            return 'This field must have ' + rDetails[1] + ' characters';
+                        }
+                        break;
 
-                  case 'email':
-                      if(input.value != '') {
-                          var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-                          if(!regex.test(input.value.toLowerCase())) {
-                              return 'This email is not valid.';
-                          }
-                      }
-                      break;
-              }
-          }
-      }
+                    case 'email':
+                        if (input.value != '') {
+                            var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+                            if (!regex.test(input.value.toLowerCase())) {
+                                return 'This email is not valid.';
+                            }
+                        }
+                        break;
+                }
+            }
+        }
 
-      return true;
-  },
-  showError:(input, error) => {
-      input.style.borderColor = '#FF0000';
+        return true;
+    },
+    showError: (input, error) => {
+        input.style.borderColor = '#FF0000';
 
-      var errorElement = document.createElement('div');
-      errorElement.classList.add('error');
-      errorElement.innerHTML = error;
+        var errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
 
-      input.parentElement.insertBefore(errorElement, input.nextElementSibling);
-  },
-  clearErrors:() => {
-      var inputs = form.querySelectorAll('input');
-      for(var i = 0; i < inputs.length; i++) {
-          inputs[i].style = '';
-      }
+        input.parentElement.insertBefore(errorElement, input.nextElementSibling);
+    },
+    clearErrors:() => {
+        var inputs = form.querySelectorAll('input');
+        for(var i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
 
-      var errorElements = document.querySelectorAll('.error');
-      for(var i = 0; i < errorElements.length; i++) {
-          errorElements[i].remove();
-      }
-  }
+        var errorElements = document.querySelectorAll('.error');
+        for(var i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
+    }
 };
 
-var form = document.querySelector('#pokevalidator');
+var form = document.getElementById("register-form");
+form.addEventListener('submit',PokeValidator.handleSubmit);
+
+var form = document.getElementById("login-form");
 form.addEventListener('submit',PokeValidator.handleSubmit);
