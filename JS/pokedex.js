9b33f9ef49b6c25cf58a18eab2db8050844ff110
Thales Felippe <thales_so@hotmail.com>
2025-05-26 21:18:22 -0300

    Implementacao das interacoes de pesquisa e refatoracao do script

--- JS/pokedex.js
+++ JS/pokedex.js
@@ -2,7 +2,6 @@ const mainScreen = document.querySelector('.display-pokemon');
 const statsScreen = document.getElementById('stats-info');
 const attacksScreen = document.getElementById('attacks-book');
 const pokeName = document.querySelector('.poke-name');
-const pokeId = document.querySelector('.poke-id');
 const pokeImage = document.querySelector('.poke-image');
 const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
@@ -19,13 +18,21 @@ const pokeSkillFour = document.querySelector('.poke-skill-four');
 const leftButton = document.querySelector('.control-left');
 const rightButton = document.querySelector('.control-right');
 const soundtrack = document.getElementById('soundtrack');
-
-const reloadPage = () => {
-    window.location.reload();
-    soundtrack.currentTime = 0;
-}
-
+const pokeIdInput = document.getElementById('poke-id-input');
+const pokeNameInput = document.getElementById('poke-name-input');
+const autocompleteList = document.getElementById('autocomplete-list');
+let allPokemonList = [];
 let searchPokemon = 1;
+
+async function loadPokemonList() {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1010');
+    const data = await res.json();
+    allPokemonList = data.results.map((poke, idx) => ({
+        name: poke.name,
+        id: idx + 1
+    }));
+}
+loadPokemonList();
 
 const TYPES = [
     'normal', 'fighting', 'flying',
@@ -41,43 +48,33 @@ const resetScreen = () => {
         pokeTypeBgOne.classList.remove(type);
         pokeTypeBgTwo.classList.remove(type);
     }
+};
+
+function showStats() {
+    attacksScreen.classList.add("hide");
+    attacksScreen.classList.remove("show");
+    statsScreen.classList.remove("hide");
+    statsScreen.classList.add("show");
 }
 
-function showStats(){
-    var element = document.getElementById("attacks-book");
-    element.classList.add("hide");
-    var element = document.getElementById("attacks-book");
-    element.classList.remove("show");
-    var element = document.getElementById("stats-info");
-    element.classList.remove("hide");
-    var element = document.getElementById("stats-info");
-    element.classList.add("show");
-}
-
-function showSkills(){
-    var element = document.getElementById("stats-info");
-    element.classList.add("hide");
-    var element = document.getElementById("stats-info");
-    element.classList.remove("show");
-    var element = document.getElementById("attacks-book");
-    element.classList.remove("hide");
-    var element = document.getElementById("attacks-book");
-    element.classList.add("show");
+function showSkills() {
+    statsScreen.classList.add("hide");
+    statsScreen.classList.remove("show");
+    attacksScreen.classList.remove("hide");
+    attacksScreen.classList.add("show");
 }
 
 const fetchPokemon = async (pokemon) => {
-
     const fetchResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (!fetchResponse.ok) return null;
     const data = await fetchResponse.json();
     return data;
-}
+};
 
 const renderPokemon = async (pokemon) => {
-
     const data = await fetchPokemon(pokemon);
 
     if (data) {
-
         resetScreen();
 
         const dataTypes = data['types'];
@@ -93,34 +90,120 @@ const renderPokemon = async (pokemon) => {
             pokeTypeTwo.textContent = '';
         }
         pokeTypeBgOne.classList.add(dataFirstType['type']['name']);
-        
+
         mainScreen.classList.remove('hide');
-        pokeName.textContent = data['name'];
+        pokeName.textContent = capitalize(data['name']);
         pokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] || data['sprites']['front_default'];
-        pokeId.textContent = 'Nº ' + data['id'].toString().padStart(3, '0');
+
+        const formattedId = 'Nº ' + data['id'].toString().padStart(3, '0');
+        pokeIdInput.value = formattedId;
+
         pokeHp.textContent = data['stats']['0']['base_stat'];
         pokeDefense.textContent = data['stats']['2']['base_stat'];
         pokeAttack.textContent = data['stats']['1']['base_stat'];
         pokeSpecialAttack.textContent = data['stats']['3']['base_stat'];
         statsScreen.classList.remove('hide');
-        pokeSkillOne.textContent = data['moves']['0']['move']['name'];
-        pokeSkillTwo.textContent = data['moves']['1']['move']['name'];
-        pokeASkillThree.textContent = data['moves']['2']['move']['name'];
-        pokeSkillFour.textContent = data['moves']['3']['move']['name'];
+        pokeSkillOne.textContent = data['moves']['0'] ? data['moves']['0']['move']['name'] : '-';
+        pokeSkillTwo.textContent = data['moves']['1'] ? data['moves']['1']['move']['name'] : '-';
+        pokeASkillThree.textContent = data['moves']['2'] ? data['moves']['2']['move']['name'] : '-';
+        pokeSkillFour.textContent = data['moves']['3'] ? data['moves']['3']['move']['name'] : '-';
         attacksScreen.classList.add('hide');
+
+        let pokeInList = allPokemonList.find(p => p.id === data.id);
+        if (pokeInList) {
+            pokeNameInput.value = `Nº ${pokeInList.id.toString().padStart(3, '0')} - ${capitalize(pokeInList.name)}`;
+        }
+    } else {
+        pokeName.textContent = 'Não encontrado';
+        pokeImage.src = '';
+        pokeIdInput.value = 'Nº ---';
+        pokeNameInput.value = '';
     }
-}
+};
+
+pokeIdInput.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        let value = this.value.replace(/[^\d]/g, '');
+        let num = parseInt(value);
+        if (!isNaN(num) && num > 0) {
+            searchPokemon = num;
+            renderPokemon(searchPokemon);
+            autocompleteList.innerHTML = '';
+            pokeNameInput.value = '';
+        } else {
+            this.classList.add('erro-id');
+            setTimeout(() => this.classList.remove('erro-id'), 600);
+        }
+    }
+});
+
+pokeIdInput.addEventListener('blur', function() {
+    let value = this.value.replace(/[^\d]/g, '');
+    let num = parseInt(value);
+    if (!isNaN(num) && num > 0) {
+        searchPokemon = num;
+        renderPokemon(searchPokemon);
+        autocompleteList.innerHTML = '';
+        pokeNameInput.value = '';
+    }
+});
+
+pokeNameInput.addEventListener('input', function() {
+    const query = this.value.trim().toLowerCase();
+    autocompleteList.innerHTML = '';
+    if (!query) return;
+
+    const filtered = allPokemonList
+        .filter(p => p.name.includes(query))
+        .slice(0, 5);
+
+    filtered.forEach(p => {
+        const paddedId = p.id.toString().padStart(3, '0');
+        const item = document.createElement('li');
+        item.textContent = `Nº ${paddedId} - ${capitalize(p.name)}`;
+        item.tabIndex = 0;
+        item.addEventListener('mousedown', function(e) {
+            searchPokemon = p.id;
+            renderPokemon(p.id);
+            pokeNameInput.value = `Nº ${paddedId} - ${capitalize(p.name)}`;
+            autocompleteList.innerHTML = '';
+        });
+        autocompleteList.appendChild(item);
+    });
+});
+
+pokeNameInput.addEventListener('keydown', function(e) {
+    const first = autocompleteList.querySelector('li');
+    if (e.key === 'Enter' && first) {
+        first.click();
+    }
+});
+
+document.addEventListener('click', function(e) {
+    if (!pokeNameInput.contains(e.target) && !autocompleteList.contains(e.target)) {
+        autocompleteList.innerHTML = '';
+    }
+});
 
 rightButton.addEventListener('click', () => {
     searchPokemon += 1;
     renderPokemon(searchPokemon);
+    pokeNameInput.value = '';
+    autocompleteList.innerHTML = '';
 });
 
 leftButton.addEventListener('click', () => {
     if (searchPokemon > 1) {
         searchPokemon -= 1;
         renderPokemon(searchPokemon);
+        pokeNameInput.value = '';
+        autocompleteList.innerHTML = '';
     }
 });
+
+function capitalize(str) {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 renderPokemon(searchPokemon);
